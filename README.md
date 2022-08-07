7bd059aef8115870593655be6d1eca7d69c875c2
lxc <1245065000@qq.com>
2022-08-07 17:55:25 +0800

    update readme and doc

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 This is a toy implementation of C++ smart pointer
+
+The design details can be seen in doc.md, 
+which is written is chinese.
+
+This project is inspired by 
+[this course](https://time.geekbang.org/column/intro/100040501?code=tiBv29BWOIyDIYtRK6GkQuMAOf0YXg8wHNp7dLpg9WQ%3D), 
+if you want to learn it for free, here is a [BaiduNetDisk link](https://pan.baidu.com/s/10AlqQObDBJcHm7EQ8uvH-g), 
+and the password is emut, all the content of this course is in it.
--- smartPtr.h
+++ smartPtr.h
@@ -3,5 +3,26 @@
 //
 #ifndef TINYSMARTPTR_SMARTPTR_H
 #define TINYSMARTPTR_SMARTPTR_H
-
+template <typename T>
+class smart_ptr {
+public:
+    explicit smart_ptr(T *ptr = nullptr): ptr_(ptr) {}
+    ~smart_ptr() {
+        delete ptr_;
+    }
+    T* get() const {
+        return ptr_;
+    }
+    T& operator *() const {
+        return *ptr_;
+    }
+    T* operator ->() const {
+        return ptr_;
+    }
+    operator bool() const {
+        return ptr_;
+    }
+private:
+    T* ptr_;
+};
 #endif //TINYSMARTPTR_SMARTPTR_H
